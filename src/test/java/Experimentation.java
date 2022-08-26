474ba7b768c7ff6a863c90d0639ecf67876f3eb3
Andreas Bade <badeand@badeand.com>
2022-08-26 19:38:35 +0200

    wip

--- src/test/java/Experimentation.java
+++ src/test/java/Experimentation.java
@@ -8,9 +8,28 @@ public class Experimentation {
     public void demoTestMethod() {
         assertTrue(true);
 
+        Card card = new Card(Suit.C, "7");
 
-
-
+        System.out.println(card);
     }
 
+    enum Suit {
+        C, D, H, S
+    }
+
+    class Card {
+        Suit   suit;
+        String value;
+
+        public Card(Suit suit, String value) {
+            this.suit = suit;
+            // TODO: Validate value
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Card{" + "suit=" + suit + ", value='" + value + '\'' + '}';
+        }
+    }
 }
